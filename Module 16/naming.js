a41d86b99ed535bca9324f9850e92a907357e540
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-01-26 02:04:19 +0600

    JS Practise added

---
+++ Module 16/naming.js
@@ -0,0 +1,32 @@
+// NAMING FORMATS
+
+// 1. You can not write a variable name similar as keyword. Like var is a keyword, true false etc.
+var var = "Mahbub"; //Not Allowed
+var true = "Mahbub"; //Not Allowed
+var Mahbub = "Mahbub"; //Allowed
+
+//2. In the name of the variable you can not provide gap.
+var my name is khan = "name what"; //not allowed
+var mynameiskhan = "name what"; //Allowed
+
+//3. The variable name can not be in quote.
+var "address" = "Baganbari"; //Not Allowed
+var address = "Baganbari" //Allowed
+
+//4. A variable name can not start with a number rather in the middle or in the end a number can be in the name.
+var Mahbub2="name"; //Allowed
+var 2Mahbub = "name"; //Not Allowed
+
+//5. Variable Name is case sensitive.
+var Mahbub;
+var MAHBUB;
+var MaHbub;
+var MaHbUb; //All the above names are different variable
+
+//6. Long Variable name can be writen as:
+var What Is Your Name = "Mahbub"; //Not allowed
+
+var WhatIsYourName = "Mahbub"; //Pascal Case
+var What_Is_Your_Name = "Mahbub"; //Snake Case
+var whatIsYourName = "Mahbub"; //Camel Case
+//In the camel case first letter is lowercase and in the pascel first letter is uppercase
---
+++ Module 16/variable.js
@@ -0,0 +1,33 @@
+// Will print 49
+console.log(49) 
+
+// Creating Variable and Storing Values
+// Keyword Name = Value;
+var Roll = 7;
+console.log(Roll);
+
+// Once created the variable the value can be replaced but do not need to declare the variable again
+Roll = 8;
+console.log(Roll);
+
+// Without providing Value a variable can be declared. If you print it will be undefined
+var StudentID;
+console.log(StudentID);
+
+// TYPE
+// Number
+var roll = 2054901049; //Number type don't require double quotation
+console.log(roll);
+console.log(typeof roll); //This will show the type of the variable roll
+
+// String
+var name = "Mahbub Sarwar"; //String type requires double quotation or single quotation
+var tag = 'Student';
+console.log(name);
+console.log(tag);
+
+// Boolean
+var MahbubIsRich = false; //Boolean type don't require double quotation and will be small caps
+var MahbubIsGorib = true;
+console.log(MahbubIsRich);
+console.log(MahbubIsGorib);
