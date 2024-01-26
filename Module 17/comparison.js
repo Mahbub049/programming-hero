e2737468c96995d7c04b4646741114a3641ac72d
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-01-26 21:34:17 +0600

    comparison added

---
+++ Module 17/comparison.js
@@ -0,0 +1,30 @@
+//Checking if 10 is large than 5 or not
+console.log(10>5);
+
+//Checking if 10 is smaller than 5 or not
+console.log(10<5);
+
+//Checking if 10 is equal to 5 or not
+console.log(10==5);
+
+//Checking if 10 is large or equal to 5 or not
+console.log(10>=5);
+
+//Checking if 10 is small or equal to 5 or not
+console.log(10<=5);
+
+//Checking if 10 is not equal to 5
+console.log(10!=5);
+
+
+//== checks the value only
+console.log(10=='10'); //True as 10 and 10 is same
+//console.log(10='Mahbub'); //Not work as Mahbub is not even a number
+
+//=== checks value as well as data type
+console.log(10==='10'); //False as data type different
+
+// Store values in variable and compare
+var midMark = 20;
+var finalMark = 80;
+console.log(midMark>finalMark);
