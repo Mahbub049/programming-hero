afdb9730bbd7d06de3ac7eaa7464058230d352d5
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-01-28 01:50:15 +0600

    array module 17 half

---
+++ Module 17/array.js
@@ -0,0 +1,13 @@
+// Declaring Array
+let roll = [49, 7, 8, 42, 2];
+console.log(roll);
+
+// Accessing Array
+console.log('Value of the indext: ' + roll[2]); //2 means index=2 that means 8
+
+// Length of the array
+console.log('The length is: ' + roll.length);
+
+//update the value of the array
+roll[4] = 3; //index 4th or last value is updated
+console.log(roll);
--- Module 17/short.js
+++ Module 17/short.js
@@ -3,18 +3,18 @@ let mahbubHaveMoney = false;
 
 //Now instead providing condition mahbubIsRich = true only mahbubIsRich will work
 
-if(mahbubIsRich){
+if (mahbubIsRich) {
     console.log("This is a lie");
 }
-else{
+else {
     console.log('Stop Dreaming');
 }
 
 //To work false condition without saying it is false in the if, use !
 
-if(!mahbubHaveMoney){ //This denots not true and mahbubHaveMoney is false so not true = false and thus resulted yeah its true
+if (!mahbubHaveMoney) { //This denots not true and mahbubHaveMoney is false so not true = false and thus resulted yeah its true
     console.log("Yeah its True");
 }
-else{
+else {
     console.log('Sleep Bro');
 }
