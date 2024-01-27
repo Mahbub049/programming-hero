eeb6a37a4b8d9084056a1b94ce39ad1cb030fdd1
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-01-27 15:29:46 +0600

    ternary and short added

---
+++ Module 17/short.js
@@ -0,0 +1,20 @@
+let mahbubIsRich = true;
+let mahbubHaveMoney = false;
+
+//Now instead providing condition mahbubIsRich = true only mahbubIsRich will work
+
+if(mahbubIsRich){
+    console.log("This is a lie");
+}
+else{
+    console.log('Stop Dreaming');
+}
+
+//To work false condition without saying it is false in the if, use !
+
+if(!mahbubHaveMoney){ //This denots not true and mahbubHaveMoney is false so not true = false and thus resulted yeah its true
+    console.log("Yeah its True");
+}
+else{
+    console.log('Sleep Bro');
+}
