4f928b88ef90e17165adcb81e9e1be772d922e4c
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-02-05 16:50:09 +0600

    math added

---
+++ Module 21/math.js
@@ -0,0 +1,20 @@
+//Minimum and Maximum in all the values
+console.log(Math.min(10, 65, 23, 67, 23, 67, 23, 54, 78, 15, 3, 65, 12));
+console.log(Math.max(10, 65, 23, 67, 23, 67, 23, 54, 78, 15, 3, 65, 12));
+
+//Pi value
+console.log(Math.PI);
+
+//Absolute Value
+console.log(Math.abs(-12));
+
+//Rounding of the value
+console.log(Math.round(3.1543543645));
+console.log(Math.round(3.6543543645));
+console.log(Math.floor(3.1543543645));
+console.log(Math.ceil(3.1543543645));
+
+//random values
+console.log(Math.random()); //0 to 1
+console.log(Math.random()*10); //1 to 10
+console.log(Math.round(Math.random()*10)); //rounded random value
