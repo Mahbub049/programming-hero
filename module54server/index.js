d95b8a7bb90356cf88d6d0bbf7b1ca8dbebd7326
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-04-22 04:20:31 +0600

    server side

---
+++ module54server/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3213;
+
+app.use(cors());
+app.use(express.json());
+
+//details
+// username: mahbubsarwar5
+// password: VYwp7asnMgjHmSGQ
+
+
+const uri = "mongodb+srv://mahbubsarwar5:VYwp7asnMgjHmSGQ@cluster0.h0zb1dz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("usersDB");
+    const users = database.collection("users");
+
+    app.post('/users', async(req, res)=>{
+        const user = req.body;
+        const result = await users.insertOne(user);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res)=>{
+    res.send("Running Module 54 Server");
+})
+
+app.listen(port, ()=>{
+    console.log('listening');
+})
